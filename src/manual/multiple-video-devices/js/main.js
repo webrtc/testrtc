37906d8934c15f9e1fbda303c7db3094f340f1ec
Christoffer Jansson <jansson@google.com>
2016-03-21 11:58:26 +0100

    fix grunt

--- src/manual/multiple-video-devices/js/main.js
+++ src/manual/multiple-video-devices/js/main.js
@@ -61,7 +61,8 @@ function getUserMediaOkCallback_(stream) {
     div.appendChild(deviceLabel);
   }
   stream.getVideoTracks()[0].addEventListener('ended', errorMessage_);
-  adapter.browserShim.attachMediaStream(document.getElementById('view' + counter), stream);
+  adapter.browserShim.attachMediaStream(
+      document.getElementById('view' + counter), stream);
   counter++;
 }
 
